263cda905227b0f2ac47bb38a702ec63b2721c2d
Vincent K <vincent.kesumo@gmail.com>
2019-04-23 14:45:02 +0200

    Added basic output log

--- src/Spotify.java
+++ src/Spotify.java
@@ -1,2 +1,12 @@
 public class Spotify {
+
+    public static void main(String args[]){
+        System.out.println("Please enter an option: ");
+        System.out.println("    1: List all songs currently added");
+        System.out.println("    2: List all songs in a playlist");
+        System.out.println("    3: Add a playlist");
+        System.out.println("    4: Add a song to playlist");
+        System.out.println("    5: Remove song/playlist");
+        System.out.println("    6: Exit Program");
+    }
 }
